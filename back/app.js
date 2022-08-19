1075198970cf28e9bd456a2ae984990cbdced7e8
wintzneo <wintzneo@gmail.com>
2022-08-19 13:51:12 +0200

    first commit

---
+++ back/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+
+//Connection à la base de donnée MongoDB
+mongoose
+  .connect(
+    "mongodb+srv://wintzneo:pm40334033@cluster0.clqmrrp.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+  
+//Header pour contourner erreurs de CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Votre requête a bien été reçue !' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Réponse envoyée avec succès !');
+});
+
+module.exports = app;
