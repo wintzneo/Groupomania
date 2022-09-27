cbb0a1d09eea19d5a873b4207cf982ae1d9dbb6d
wintzneo <wintzneo@gmail.com>
2022-09-27 11:43:15 +0200

    redesign du like

--- front/src/components/Likes/Likes.jsx
+++ front/src/components/Likes/Likes.jsx
@@ -1,74 +1,31 @@
-import React, { useCallback, useState } from 'react'
-import { AiFillHeart } from 'react-icons/ai'
-import axios from 'axios'
+import React, { useState } from "react";
+import cn from "classnames";
+import { ReactComponent as Heart } from "../Likes/red-heart-icon.svg";
 
-const LikeButton = (item) => {
-  const [like, setLike] = useState(1),
-    [isLike, setIsLike] = useState(-1),
-    onLikeClick = async () => {
-      setLike(like + (isLike ? -1 : 1))
-      setIsLike(!isLike)
-      LikePost()
-    }
-  /* 
-  onDeleteLikeClick = async () => {
-    setLike(like + (isLike ? -1 : 1))
-    setIsLike(!isLike)
-    LikeDelete()
-  } */
+const particleList = Array.from(Array(10));
 
-  const postId = item.id
-
-  //Ajouter un like
-  const LikePost = useCallback(async () => {
-    await axios.get(`http://localhost:4200/api/posts/${postId}`, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-      },
-    })
-    try {
-      await axios.post(`http://localhost:4200/api/posts/${postId}/likes`, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-        },
-      })
-    } catch (error) {
-      alert('error')
-      console.log(error)
-    }
-  }, [postId])
-
-  //Supprimer Like
-  const LikeDelete = useCallback(async () => {
-    await axios.get(`http://localhost:4200/api/posts/${postId}`, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-      },
-    })
-    try {
-      await axios.delete(`http://localhost:4200/api/posts/${postId}/likes`, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-        },
-      })
-    } catch (error) {
-      alert('error')
-      console.log(error)
-    }
-  }, [postId])
+const LikeButton = () => {
+  const [liked, setLiked] = useState(null);
+  const [clicked, setClicked] = useState(false);
 
   return (
-    <>
-      <div className="like">
-        <p onClick={onLikeClick} className="icon-heart">
-          <AiFillHeart />
-        </p>
-        <p className="likes_length">
-          {'Like'} | {like}
-        </p>
+    <button
+      onClick={() => {
+        setLiked(!liked);
+        setClicked(true);
+      }}
+      onAnimationEnd={() => setClicked(false)}
+      className={cn("like-button-wrapper", {
+        liked,
+        clicked,
+      })}
+    >
+      <div className="like-button">
+        <Heart />
+        <span className={cn("suffix", { liked })}>Liké !</span>
       </div>
-    </>
-  )
-}
+    </button>
+  );
+};
 
-export default LikeButton
+export default LikeButton;
